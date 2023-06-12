7bb5f1c55dc846480c5d246d68eff4b1e858ed2e
victor diaz <vdmpeniel@yahoo.es>
2023-06-12 12:23:49 -0400

    Added all required changes.

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,25 @@
 package lang.print.gaps.finalModuleTask;
 
+
 public class DigitsSumCalculator {
+
+
+
+    private void digitSum2(int number){
+        int length = 10000;
+        int sum = 0;
+        do{
+            length /= 10;
+            sum += number / length;
+            number = number % length;
+        } while(length > 1);
+        System.out.println(sum);
+    }
+
     public void calculateSum(int number) {
+        digitSum2(number);
     }
 }
+
+
+
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println(Integer.valueOf(new StringBuilder(String.valueOf(number)).reverse().toString()));
+
     }
 }
